b9c49d01161ac4b4f36ad23582e2aede41671bc2
aelsam2 <aelsam2@gmail.com>
2021-03-03 10:38:02 +0530

    initial commit

--- cypress/integration/RegressionSuit/FileUpload.spec.js
+++ cypress/integration/RegressionSuit/FileUpload.spec.js
@@ -6,15 +6,17 @@ describe("login functionality", function(){
 it ("login with valid credentials", function()
 { cy.visit('/')
 cy.get('.ng-tns-c70-2:nth-child(2)').click();
+cy.wait(10000)
 cy.get('#mat-option-3 > .mat-option-text').click();
 cy.get('.ng-tns-c70-4:nth-child(2)').click();
+cy.wait(10000)
 cy.get('#mat-option-38 > .mat-option-text').click();
 cy.get('.fillBtn').click();
 
 cy.get('[title="Test 2"]').click();
-cy.wait(5000)
+cy.wait(10000)
 cy.get(':nth-child(2) > .step-circle-container > .tick-image').click();
-cy.wait(5000)
+cy.wait(10000)
 cy.get('.upload-link').click();
 
 
